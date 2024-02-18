19e818e52d22156ef778db5b6807e6052134c6ed
yabu132 <yara.abusaude@gmail.com>
2024-02-17 23:33:07 -0800

    git push

--- src/components/schedule/Schedule.jsx
+++ src/components/schedule/Schedule.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './Schedule.css'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Schedule.css";
+import Calendar from "react-calendar";
 
 const Schedule = () => {
   return (
-    <div>Schedule</div>
-  )
-}
+    <div>
+      <h1>Schedule</h1>
+      <Calendar />
+      <div class="weekOne"></div>
+    </div>
+  );
+};
 
-export default Schedule
+export default Schedule;
