cd4ea33582a911f09ee15d7361c5c20bc0e47c82
Revati Tambe <24revati@gmail.com>
2024-02-17 20:08:43 -0800

    intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,18 @@ import './Intro.css'
 
 const Intro = () => {
   return (
-    <div>Intro</div>
+	<div className='intro'>
+		<img src="/header.png" alt="" className='header-img'/>
+		<div className="intro-info">
+			<h2>Easy Ridesharing.</h2>
+			<h1>CarPOW!</h1>
+			<div className="intro-info-buttons">
+				<div className="btn">SSO</div>
+				<div className="btn btn-primary">Sign In</div>
+			</div>
+			<p style={{color:"white"}}>Don't have an account? <span style={{fontWeight:"600"}}>Sign Up</span></p>
+		</div>
+	</div>
   )
 }
 
