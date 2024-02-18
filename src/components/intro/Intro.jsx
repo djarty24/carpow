c4e7a87039c59d4927c7c2f7247c6ce4818f4c44
Revati Tambe <24revati@gmail.com>
2024-02-17 20:17:45 -0800

    finished intro page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Intro.css'
 
 const Intro = () => {
@@ -9,10 +10,10 @@ const Intro = () => {
 			<h2>Easy Ridesharing.</h2>
 			<h1>CarPOW!</h1>
 			<div className="intro-info-buttons">
-				<div className="btn">SSO</div>
-				<div className="btn btn-primary">Sign In</div>
+				<div className="btn btn-secondary">SSO</div>
+				<Link to={"/login"} style={{textDecoration:"none"}}><div className="btn btn-primary">Log In</div></Link>	
 			</div>
-			<p style={{color:"white"}}>Don't have an account? <span style={{fontWeight:"600"}}>Sign Up</span></p>
+			<p style={{color:"white", fontSize:"small", textAlign:"center"}}>Don't have an account? <span style={{fontWeight:"600"}}>Sign Up</span></p>
 		</div>
 	</div>
   )
