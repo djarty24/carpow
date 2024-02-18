54c869b02af47b00efa4811b770e1ca4fed34ea2
Revati Tambe <24revati@gmail.com>
2024-02-17 22:42:51 -0800

    groups

--- src/components/groups/Groups.jsx
+++ src/components/groups/Groups.jsx
@@ -3,9 +3,34 @@ import { Link } from 'react-router-dom'
 import './Groups.css'
 
 const Groups = () => {
-  return (
-    <div>Groups</div>
-  )
+	return (
+		<div className='groups'>
+			<div className="groups-title">
+				<div className="groups-title-left">
+					<h2>Your Carpool</h2>
+					<h1>Groups</h1>
+				</div>
+				<img src="groups-title.png" alt="" className="groups-title-right" />
+			</div>
+			<hr style={{border:"5px solid #F2F8F8", borderRadius:"5px", marginBottom:"40px"}} />
+			<div className="groups-groups">
+				<div className="groups-group">
+					<img src="group1.png" alt="" />
+					<div className="groups-group-info">
+						<h4>Homestead Kids</h4>
+						<p>Steve Jobs, Linus Torvalds, Richard Feynman, Stephen Hawking</p>
+					</div>
+				</div>
+				<div className="groups-group">
+					<img src="group2.png" alt="" />
+					<div className="groups-group-info">
+						<h4>SMS Carpool</h4>
+						<p>Dennis Ritchie, Monty Python, Steve Wozniack, Joe Biden</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Groups
