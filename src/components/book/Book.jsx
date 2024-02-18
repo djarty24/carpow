4feef86e497eec15c2afb83d5db74a32e8949c66
yabu132 <yara.abusaude@gmail.com>
2024-02-17 20:09:05 -0800

    s

--- src/components/book/Book.jsx
+++ src/components/book/Book.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import "./Book.css";
 
 const Book = () => {
@@ -6,16 +6,16 @@ const Book = () => {
     <div className="book">
       <h1>Sign Up For A Slot</h1>
       <div class="dropdown">
-  <button class="dropbtn">Dropdown</button>
-  <div class="dropdown-content">
-    <a href="#">Link 1</a>
-    <a href="#">Link 2</a>
-    <a href="#">Link 3</a>
-  </div>
-</div>
-      <button type="button" class="btn btn-outline-primary">Book Now!</button>
+        <button class="dropbtn">Dropdown</button>
+        <div class="dropdown-content">
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </div>
+      <button>Book Now!</button>
     </div>
-  )
-}
+  );
+};
 
-export default Book
+export default Book;
