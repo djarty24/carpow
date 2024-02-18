5139aeac02f643b937650240bae9733d5f301835
Revati Tambe <24revati@gmail.com>
2024-02-18 02:48:04 -0800

    working on calendar

--- src/components/schedule/Schedule.jsx
+++ src/components/schedule/Schedule.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
+import Calendar from 'react-calendar';
 import { Link, useParams } from 'react-router-dom'
 
 import './Schedule.css'
 
 const Schedule = () => {
-	
+
 	const { id } = useParams();
-	let schedule = id.replace("-"," ");
+	let schedule = id.replace("-", " ");
+	const customWeekdayFormat = (locale, date) => {
+        return ['S', 'M', 'T', 'W', 'T', 'F', 'S'][date.getDay()];
+    };
 
 	return (
 		<div className='schedule'>
-			<h1>{ schedule }<br/>Carpool Schedule</h1>
+			<h1>{schedule}<br />Carpool Schedule</h1>
+			<Calendar 
+			locale="en-US"
+			formatShortWeekday={customWeekdayFormat}
+			/>
 		</div>
 	)
 }
