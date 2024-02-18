e085f9caf281c5c00a884c08b0541fa0d69b816c
Revati Tambe <24revati@gmail.com>
2024-02-18 03:19:27 -0800

    some scheduling

--- src/components/schedule/Schedule.jsx
+++ src/components/schedule/Schedule.jsx
@@ -6,23 +6,61 @@ import './Schedule.css'
 
 const Schedule = () => {
 
-	const { id } = useParams();
-	let schedule = id.replace("-", " ");
+    const { id } = useParams();
+    let schedule = id.replace("-", " ");
 
-	const customWeekdayFormat = (locale, date) => {
+    const customWeekdayFormat = (locale, date) => {
         return ['S', 'M', 'T', 'W', 'T', 'F', 'S'][date.getDay()];
     };
 
-	return (
-		<div className='schedule'>
-			<h1>{schedule}<br />Carpool Schedule</h1>
-            <hr style={{ border: "5px solid #F2F8F8", borderRadius: "5px", margin:"5px 25px" }} />
-			<Calendar 
-			locale="en-US"
-			formatShortWeekday={customWeekdayFormat}
-			/>
-		</div>
-	)
+    return (
+        <div className='schedule'>
+            <h1>{schedule}<br />Carpool Schedule</h1>
+            <hr style={{ border: "5px solid #F2F8F8", borderRadius: "5px", margin: "5px 25px" }} />
+            <Calendar
+                locale="en-US"
+                formatShortWeekday={customWeekdayFormat}
+            />
+            <div className="weeks">
+                <div className="weeks-week">
+                    <div className="weeks-week-info">
+                        <div>
+                            <h4>Week 1</h4>
+                            <h6>1/29/24 - 2/2/24</h6>
+                        </div>
+                        <p>Slots Remaining</p>
+                    </div>
+                </div>
+                <div className="weeks-week">
+                    <div className="weeks-week-info">
+                        <div>
+                            <h4>Week 2</h4>
+                            <h6>1/29/24 - 2/2/24</h6>
+                        </div>
+                        <p>Full</p>
+                    </div>
+                </div>
+                <div className="weeks-week">
+                    <div className="weeks-week-info">
+                        <div>
+                            <h4>Week 3</h4>
+                            <h6>1/29/24 - 2/2/24</h6>
+                        </div>
+                        <p>Full</p>
+                    </div>
+                </div>
+                <div className="weeks-week">
+                    <div className="weeks-week-info">
+                        <div>
+                            <h4>Week 4</h4>
+                            <h6>1/29/24 - 2/2/24</h6>
+                        </div>
+                        <p>Slots Remaining</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Schedule
