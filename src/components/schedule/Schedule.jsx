2cf4769675056d7215e4476f48c556e8e3f60b45
Revati Tambe <24revati@gmail.com>
2024-02-18 03:06:08 -0800

    calendar

--- src/components/schedule/Schedule.jsx
+++ src/components/schedule/Schedule.jsx
@@ -8,6 +8,7 @@ const Schedule = () => {
 
 	const { id } = useParams();
 	let schedule = id.replace("-", " ");
+
 	const customWeekdayFormat = (locale, date) => {
         return ['S', 'M', 'T', 'W', 'T', 'F', 'S'][date.getDay()];
     };
@@ -15,6 +16,7 @@ const Schedule = () => {
 	return (
 		<div className='schedule'>
 			<h1>{schedule}<br />Carpool Schedule</h1>
+            <hr style={{ border: "5px solid #F2F8F8", borderRadius: "5px", margin:"5px 25px" }} />
 			<Calendar 
 			locale="en-US"
 			formatShortWeekday={customWeekdayFormat}
