caf0966d7f2c17748d4636b27ba7f90f84dc5a6b
Revati Tambe <24revati@gmail.com>
2024-02-18 03:49:30 -0800

    popups!

--- src/components/schedule/Schedule.jsx
+++ src/components/schedule/Schedule.jsx
@@ -1,67 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Calendar from 'react-calendar';
 import { Link, useParams } from 'react-router-dom'
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+import CustomPopup from './CustomPopup';
 
 import './Schedule.css'
 
 const Schedule = () => {
 
-    const { id } = useParams();
-    let schedule = id.replace("-", " ");
+	const { id } = useParams();
+	let schedule = id.replace("-", " ");
 
-    const customWeekdayFormat = (locale, date) => {
-        return ['S', 'M', 'T', 'W', 'T', 'F', 'S'][date.getDay()];
-    };
+	const customWeekdayFormat = (locale, date) => {
+		return ['S', 'M', 'T', 'W', 'T', 'F', 'S'][date.getDay()];
+	};
 
-    return (
-        <div className='schedule'>
-            <h1>{schedule}<br />Carpool Schedule</h1>
-            <hr style={{ border: "5px solid #F2F8F8", borderRadius: "5px", margin: "5px 25px" }} />
-            <Calendar
-                locale="en-US"
-                formatShortWeekday={customWeekdayFormat}
-            />
-            <hr style={{ border: "5px solid #F2F8F8", borderRadius: "5px", margin: "5px 25px" }} />
-            <div className="weeks">
-                <div className="weeks-week">
-                    <div className="weeks-week-info">
-                        <div>
-                            <h4>Week 1</h4>
-                            <h6>1/29/24 - 2/2/24</h6>
-                        </div>
-                        <p>Slots Remaining</p>
-                    </div>
-                </div>
-                <div className="weeks-week">
-                    <div className="weeks-week-info">
-                        <div>
-                            <h4>Week 2</h4>
-                            <h6>1/29/24 - 2/2/24</h6>
-                        </div>
-                        <p>Full</p>
-                    </div>
-                </div>
-                <div className="weeks-week">
-                    <div className="weeks-week-info">
-                        <div>
-                            <h4>Week 3</h4>
-                            <h6>1/29/24 - 2/2/24</h6>
-                        </div>
-                        <p>Full</p>
-                    </div>
-                </div>
-                <div className="weeks-week">
-                    <div className="weeks-week-info">
-                        <div>
-                            <h4>Week 4</h4>
-                            <h6>1/29/24 - 2/2/24</h6>
-                        </div>
-                        <p>Slots Remaining</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+	const [selectedDate, setSelectedDate] = useState(null);
+
+	const handleTileClick = (value) => {
+		setSelectedDate(value);
+	};
+
+	const handleClosePopup = () => {
+		setSelectedDate(null);
+	};
+
+	return (
+		<div className='schedule'>
+			<h1>{schedule}<br />Carpool Schedule</h1>
+			<hr style={{ border: "5px solid #F2F8F8", borderRadius: "5px", margin: "5px 25px" }} />
+			<Calendar
+				locale="en-US"
+				formatShortWeekday={customWeekdayFormat}
+				tileClassName={({ date }) => date === selectedDate ? 'selected' : null}
+				onClickDay={handleTileClick}
+			/>
+			
+			<CustomPopup selectedDate={selectedDate} onClose={handleClosePopup} />
+
+			<hr style={{ border: "5px solid #F2F8F8", borderRadius: "5px", margin: "5px 25px" }} />
+			<div className="weeks">
+				<div className="weeks-week">
+					<div className="weeks-week-info">
+						<div>
+							<h4>Week 1</h4>
+							<h6>1/29/24 - 2/2/24</h6>
+						</div>
+						<p>Slots Remaining</p>
+					</div>
+				</div>
+				<div className="weeks-week">
+					<div className="weeks-week-info">
+						<div>
+							<h4>Week 2</h4>
+							<h6>1/29/24 - 2/2/24</h6>
+						</div>
+						<p>Full</p>
+					</div>
+				</div>
+				<div className="weeks-week">
+					<div className="weeks-week-info">
+						<div>
+							<h4>Week 3</h4>
+							<h6>1/29/24 - 2/2/24</h6>
+						</div>
+						<p>Full</p>
+					</div>
+				</div>
+				<div className="weeks-week">
+					<div className="weeks-week-info">
+						<div>
+							<h4>Week 4</h4>
+							<h6>1/29/24 - 2/2/24</h6>
+						</div>
+						<p>Slots Remaining</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Schedule
